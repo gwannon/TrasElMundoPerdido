e3f671a91abaf7eb52397730c7498379d450b390
Gwannon <monclus.jorge@gmail.com>
2024-06-07 11:41:03 +0200

    Mejoras script de generación de PDF

---
+++ tools/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$tags = [
+  'HTML' => '',
+  'HASH' => date("YmdHis"),
+  'TITLE' => "Tras el mundo perdido",
+  'SHORTDESCRIPTION' => 'Una campaña donde lucharás contra nazis a lomos de dinosaurios.',
+  'DESCRIPTION' => "Tras El Mundo Perdido es una campaña para SWADE con la que podrás jugar aventuras durante la Segunda Guerra Mundial con un comando del ejército inglés que usa dinosaurios en sus misiones contra los nazis.",
+  'VERSION' => "0.1",
+  "AUTHOR" => "@Gwannon",
+  "KEYWORDS" => "juego de rol, Savage Worlds, dinosaurios, nazis, II Guerra Mundial"
+];
