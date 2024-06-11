af64e7210a5ba450918e39ae2ef63a7d1ac388ca
Gwannon <monclus.jorge@gmail.com>
2024-06-11 14:16:47 +0200

    Minor fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tras El Mundo Perdido
-"Tras El Mundo Perdido" es una ambientación para SWADE con el que podrás jugar aventuras durante la Segunda Guerra Mundial con un comando del ejército inglés que usa dinosaurios en sus misiones contra los nazis.
+"OPERACIÓN: Tras El Mundo Perdido" es una ambientación para SWADE con el que podrás jugar aventuras durante la Segunda Guerra Mundial con un comando del ejército inglés que usa dinosaurios en sus misiones contra los nazis.
 
 [Página web](https://mundoperdido.gwannon.com/)
 
--- tools/config.php
+++ tools/config.php
@@ -3,10 +3,12 @@
 $tags = [
   'HTML' => '',
   'HASH' => date("YmdHis"),
-  'TITLE' => "Tras el mundo perdido",
+  'TITLE' => "OPERACIÓN: Tras el mundo perdido",
   'SHORTDESCRIPTION' => 'Una campaña donde lucharás contra nazis a lomos de dinosaurios.',
-  'DESCRIPTION' => "Tras El Mundo Perdido es una campaña para SWADE con la que podrás jugar aventuras durante la Segunda Guerra Mundial con un comando del ejército inglés que usa dinosaurios en sus misiones contra los nazis.",
+  'DESCRIPTION' => "OPERACIÓN: Tras El Mundo Perdido es una campaña para el juego de rol Savage Worlds con la que podrás jugar aventuras durante la Segunda Guerra Mundial con un comando del ejército inglés que usa dinosaurios en sus misiones contra los nazis.",
   'VERSION' => "0.2",
   "AUTHOR" => "@Gwannon",
-  "KEYWORDS" => "juego de rol, Savage Worlds, dinosaurios, nazis, II Guerra Mundial"
+  "KEYWORDS" => "juego de rol, Savage Worlds, dinosaurios, nazis, II Guerra Mundial",
+  "URL" => "https://mundoperdido.gwannon.com",
+  "GITHUB" => 'https://github.com/gwannon/TrasElMundoPerdido'
 ];
